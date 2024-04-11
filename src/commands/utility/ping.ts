1130bf65215250ef618e5a9bb5d504aee29c2e34
Joseph Umana <joseph@josephumana.com>
2024-04-10 22:06:22 -0400

    Changed formatting
    Signed-off-by: Joseph Umana <joseph@josephumana.com>

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -2,11 +2,11 @@ import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
 import { Command } from '../../types';
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Returns your ping'),
-  async execute(interaction: ChatInputCommandInteraction) {
-    // This returns API latency
-    await interaction.reply(`Pong ${interaction.client.ws.ping}ms`);
-  },
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Returns your ping'),
+    async execute(interaction: ChatInputCommandInteraction) {
+        // This returns API latency
+        await interaction.reply(`Pong ${interaction.client.ws.ping}ms`);
+    },
 } as Command;
