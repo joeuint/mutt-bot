7cd1f52a3c934d8b7d7187cc7a0f311bac869f49
Joseph Umana <joseph@josephumana.com>
2024-04-10 22:08:42 -0400

    Improve core error handling
    Signed-off-by: Joseph Umana <joseph@josephumana.com>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import {
-  Client,
-  GatewayIntentBits,
-  Events,
-  Collection,
-  Interaction,
+    Client,
+    GatewayIntentBits,
+    Events,
+    Collection,
+    Interaction,
+    DiscordAPIError,
 } from 'discord.js';
 import { fileURLToPath } from 'url';
 import { Command } from './types';
@@ -12,44 +13,44 @@ import 'dotenv/config';
 import fs from 'fs';
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
 });
 
 client.on(Events.ClientReady, (readyClient) => {
-  console.log(
-    `Logged in as ${readyClient.user.username} (${readyClient.user.id})!`
-  );
+    console.log(
+        `Logged in as ${readyClient.user.username} (${readyClient.user.id})!`
+    );
 });
 
 client.on(Events.GuildMemberAdd, (event) => {
-  console.log(`${event.user.username} joined ${event.guild.name}`);
+    console.log(`${event.user.username} joined ${event.guild.name}`);
 });
 
 client.on(Events.InteractionCreate, async (interaction: Interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
-  const command = interaction.client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName);
 
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName}`);
-    return;
-  }
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: 'There was an error while executing this command!',
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: 'There was an error while executing this command!',
-        ephemeral: true,
-      });
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName}`);
+        return;
     }
-  }
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: `There was an error (${error instanceof DiscordAPIError ? error.message : ''}) while executing this command!`,
+                ephemeral: true,
+            });
+        } else {
+            await interaction.reply({
+                content: `There was an error (${error instanceof DiscordAPIError ? error.message : ''}) while executing this command!`,
+                ephemeral: true,
+            });
+        }
+    }
 });
 
 client.commands = new Collection();
@@ -63,31 +64,31 @@ const commandFolders = fs.readdirSync(baseFolder);
 console.log(`mutt-bot ${process.env.npm_package_version}`);
 
 for (const folder of commandFolders) {
-  console.log(`Loading ${folder} commands`);
+    console.log(`Loading ${folder} commands`);
 
-  const commandPath = path.join(baseFolder, folder);
-  if (!fs.lstatSync(commandPath).isDirectory()) {
-    console.warn(`${commandPath} is not a directory! Skipping...`);
-    continue;
-  }
-
-  const commandFiles = fs
-    .readdirSync(commandPath)
-    // Should work as we are compiling the ts first, instead of running the ts directly
-    .filter((s) => s.endsWith('.js'));
-
-  for (const file of commandFiles) {
-    const filePath = path.join(commandPath, file);
-    const command: Command = (await import(filePath)).default;
-
-    if ('data' in command && 'execute' in command) {
-      client.commands.set(command.data.name, command);
-      console.log(`Loaded ${file}`);
-    } else {
-      console.warn(`${filePath} is missing data and execute properties!`);
+    const commandPath = path.join(baseFolder, folder);
+    if (!fs.lstatSync(commandPath).isDirectory()) {
+        console.warn(`${commandPath} is not a directory! Skipping...`);
+        continue;
     }
-  }
-  console.log(`Finished loading ${folder} commands`);
+
+    const commandFiles = fs
+        .readdirSync(commandPath)
+        // Should work as we are compiling the ts first, instead of running the ts directly
+        .filter((s) => s.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandPath, file);
+        const command: Command = (await import(filePath)).default;
+
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+            console.log(`Loaded ${file}`);
+        } else {
+            console.warn(`${filePath} is missing data and execute properties!`);
+        }
+    }
+    console.log(`Finished loading ${folder} commands`);
 }
 
 client.login(process.env.DISCORD_BOT_TOKEN);
